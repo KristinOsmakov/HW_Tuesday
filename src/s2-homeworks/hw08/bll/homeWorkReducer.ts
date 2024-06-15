a31973e1bb16fe8adfee13526af3116d8da6d689
KristinOsmakov <oskristinov@gmail.com>
2024-06-15 16:29:57 +0300

    Up! Add HW8. Wednesday, week 4.

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,18 +1,27 @@
 import {UserType} from '../HW8'
+import User from "../User";
+import React from "react";
 
 type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            const sortedState = [...state].sort((a, b) => {
+                if (a.name > b.name) return action.payload === 'up' ? 1 : -1;
+                if (a.name < b.name) return action.payload === 'up' ? -1 : 1;
+                return 0;
+            });
+            return sortedState;
 
-            return state // need to fix
+
+            // need to fix
         }
         case 'check': {
 
-            return state // need to fix
+            return state.filter(el => el.age >= 18) // need to fix
         }
         default:
             return state
