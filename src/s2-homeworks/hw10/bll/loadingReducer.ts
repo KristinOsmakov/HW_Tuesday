0efb7ad319f255753fccc24237f559aac3c61e7c
KristinOsmakov <oskristinov@gmail.com>
2024-07-01 13:39:15 +0300

    Up! Add HW9. Thursday, week 2.

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,10 +1,19 @@
 const initState = {
     isLoading: false,
 }
+export type InitStateType = {
+    isLoading: boolean
+}
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state: InitStateType = initState, action: LoadingActionType): InitStateType => { // fix any
     switch (action.type) {
         // пишет студент  // need to fix
+        case 'CHANGE_LOADING': {
+            return {
+                ...state,
+                isLoading: action.isLoading
+            }
+        }
 
         default:
             return state
