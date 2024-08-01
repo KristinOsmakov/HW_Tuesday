8de0fbca6480fece081d35696184ffcf25ecf59e
KristinOsmakov <oskristinov@gmail.com>
2024-08-01 15:24:06 +0300

    Up! Add HW12. Thursday, week 4. Trial.

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,27 @@
-const initState = {
+const initState: initStateType = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+type initStateType = {
+    themeId: number,
+}
+type ChangeThemeAction = {
+    type: 'SET_THEME_ID',
+    id: number
+}
+
+
+export const themeReducer = (state = initState, action: ChangeThemeAction): initStateType => { // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID':
+            return {...state, themeId: action.id}
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export const changeThemeId = (id: number): ChangeThemeAction => ({ type: 'SET_THEME_ID', id }) // fix any
+
+
+
